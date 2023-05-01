2a47606766d9b0c1435065239c9e23727c5aa1eb
thanhluong <doannguyenthanhluong@gmail.com>
2023-05-01 23:13:36 +0700

    Remove secret key

--- nbkbotapi/settings.py
+++ nbkbotapi/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7sfce8w)*mr+p%s*)7$_n^v4b4%c%$$^wlyh(&)o1#*vhbtl$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
